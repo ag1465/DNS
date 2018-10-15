aadc71983b30731eaf59c9c87ad29696dd69ce1c
ag1465 <33241158+ag1465@users.noreply.github.com>
2018-10-14 23:38:41 -0400

    Commented rss and client

--- client.py
+++ client.py
@@ -2,26 +2,26 @@ import socket as mysoc
 import numpy as mypy
 def client():
     try:
-    #[ first socket]
+    #[Create The First Socket]
         ctors=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
     except mysoc.error as err:
         print('{} \n'.format("socket open error ",err))
-    #[second socket]
-	
+    #[Create The Second Socket]
     try:
         ctots=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
     except mysoc.error as err:
         print('{} \n'.format("socket open error ", err))
-
+    #Define the port and get hostname for local machine
     port = 50007
     sa_sameas_myaddr = mysoc.gethostbyname(mysoc.gethostname())
+    #Connect to the RS server on the local machine
     server_binding=(sa_sameas_myaddr,port)
     ctors.connect(server_binding)
     #First Connect to RS server
     hostnames_arr = []
-    #write to output
+    #Open file to write
     with open('RESOLVED.txt', 'w') as output_file:
-        #read file
+        #Open file to read the host name
         with open('PROJI-HNS.txt', 'r') as input_file:
             for line in input_file:
                 hostnames_arr.append(line) #reads hostname to list
@@ -31,29 +31,45 @@ def client():
         for i in range(length):
             hostname = hostnames_arr[i]
             print('send ' + hostname)
-            ctors.send(hostname.encode('utf-8')) #send hostname to server
-            dr=ctors.recv(1024).decode('utf-8') #receive back A or NS
+            #encode hostname string and send to RS server
+            ctors.send(hostname.encode('utf-8'))
+            #decode string that is received by RS server
+            dr=ctors.recv(1024).decode('utf-8') 
             print(dr)
+            #Split the string into array to get hostname, ip address, and flag
             data = dr.split()
+            #check if flag is A or NS
             if data[2] == 'A':
+                #If A, write to output file
                 output_file.write(dr.strip() + '\n')
             elif data[2] == 'NS':
+                #If NS, send hostname to TS Server by using hostname given by RS server. 
                 TSname = data [0]
+                #bind the socket to the given TS hostname and port
                 server_binding=(TSname, 50008)
-                
+                #First time we would connect to the TS server and then send hostname.
+                #Second time we see that there is a connection so we just send the hostname.
                 try:
                     ctots.send(hostname.encode('utf-8'))
                 except:
                     ctots.connect(server_binding)
+                    #encode hostname string and send to TS server
                     ctots.send(hostname.encode('utf-8'))
+                #decode string that is received by TS server.
                 dr2 = ctots.recv(1024).decode('utf-8')
-                data2 = dr2.split()
-                if data2[2] == 'A': 
+                #Split the string into array to get hostname, ip address, and flag
+                data2 = dr2.split() 
+                #check if flag is A or HostNotFound
+                if data2[2] == 'A':
+                    #If A, write to output file
                     output_file.write(dr2.strip() + '\n')
-                else:	
+                else:
+                    #If HostNotFound, write Hostname - Error:HOST NOT FOUND
                     output_file.write(dr2.strip() + '\n')
                     print('Hostname - Error:HOST NOT FOUND')
+    # close the client socket to RS and TS  and exit 
     ctors.close()
     ctots.close()
     exit()
+#call client
 client()
--- rss.py
+++ rss.py
@@ -1,26 +1,36 @@
 import socket as mysoc
 import numpy as mypy
 def rss():
+    #Create the socket
     try:
         rssd = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
     except mysoc.error as err:
         print('[RS]{} \n'.format("RS server socket open error ", err))
+    #Bind the socket to my address and defined port
     server_binding=('',50007)
     rssd.bind(server_binding)
     #Listen for one connection at a time
     rssd.listen(1)
     #Wait for connection from client socket
     crsd,addr = rssd.accept()
-    RS_table = []
+    #Initialize lists that will collectively store the hostanmes, IP addresses, and Flag of each entry
+    RS_table = [] # list of strings from the file
     RS_table_host = []
     RS_table_ip = []
     RS_table_flag = []
+    #open file to read the hostnames, IP addresses, and Flags
     with open('PROJI-DNSRS.txt', 'r') as input_file:
+        #go through input file line by line
         for line in input_file:
+            #add to list of strings
             RS_table.append(line)
+            #splits the string into the fields: hostnames, IP address, and Flag
             data = line.split()
+            #add to list of hostnames
             RS_table_host.append(data[0])
+            #add to list of IP addresses
             RS_table_ip.append(data[1])
+            #add to list of flags
             RS_table_flag.append(data[2])
             print(data[0])
             print(data[1])
@@ -28,20 +38,33 @@ def rss():
         print(RS_table_host)
         print(RS_table_ip)
         print(RS_table_flag)
+    #wait for messages
     while True:
+        #receive hostname client and decode to string form. Strip off white space on the beginning and end of the string.
         hnstring = crsd.recv(1048).decode('utf-8').strip()
         print('this is ' + '<'  + hnstring + '>')
+   #checks if the hostname exist in the list of hostnames
+        #host name exist
         if hnstring in RS_table_host:
             print('Found')
             print(RS_table_host.index(hnstring))
+            #find the index of the hostname
             index = RS_table_host.index(hnstring)
+            #use the index and grab the string
             entry = RS_table[index]
+            #encode the string and send back to the client
             crsd.send(entry.encode('utf-8'))
+        #host name does not exist
         else:
             print('Not HERE')
+            #find the index of flag 'NS'
             index = RS_table_flag.index('NS')
+            #use the index and grab the string
             entry = RS_table[index]
+            #encode the NS string and send back to the client with the TS server hostname
             crsd.send(entry.encode('utf-8'))
+    #close the server socket and exit
     rssd.close()
     exit()
+#call server
 rss()
